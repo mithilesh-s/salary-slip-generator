19d330d87fb8246c495037b11005a46031d188ed
mithilesh <mithilesh.s@codestoresolutions.com>
2022-10-03 15:11:11 +0530

    Test cases for mat menu

--- salary-slip-generator/src/modules/employee/components/mat-menu-test/mat-menu-test.component.spec.ts
+++ salary-slip-generator/src/modules/employee/components/mat-menu-test/mat-menu-test.component.spec.ts
@@ -1,36 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
-import { By } from '@angular/platform-browser';
-
 import { MatMenuTestComponent } from './mat-menu-test.component';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatMenuHarness} from '@angular/material/menu/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('MatMenuTestComponent', () => {
+let loader: HarnessLoader;
+
+fdescribe('MatMenuTestComponent', () => {
   let component: MatMenuTestComponent;
   let fixture: ComponentFixture<MatMenuTestComponent>;
-  let dom;
-  let button;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MatMenuTestComponent ],
-      imports:[
-        MatMenuModule
+      declarations: [MatMenuTestComponent],
+      imports: [
+        MatMenuModule,
+        NoopAnimationsModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(MatMenuTestComponent);
     component = fixture.componentInstance;
-    dom = fixture.nativeElement;
-    button = dom.querySelector('#userMenu');
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  fit('should not have the menu open', async () => {
-    const menu = dom.parentNode.querySelector('.mat-menu-panel');
-    expect(menu).toBeFalsy();
+
+  it('should have two mat menu', async() => {
+  
+    const menus = await loader.getAllHarnesses(MatMenuHarness);
+    expect(menus.length).toBe(2);
+   
+  })
+
+  it('should get menu text', async () => {
+    const [firstMenu, secondMenu] = await loader.getAllHarnesses(MatMenuHarness);
+    expect(await firstMenu.getTriggerText()).toBe('Settings');
+    expect(await secondMenu.getTriggerText()).toBe('Disabled menu');
   });
 
-});
+  it('should open and close', async () => {
+    const menu = await loader.getHarness(MatMenuHarness.with({triggerText: 'Settings'}));
+    expect(await menu.isOpen()).toBe(false);
+    await menu.open();
+    expect(await menu.isOpen()).toBe(true);
+    await menu.close();
+    expect(await menu.isOpen()).toBe(false);
+  });
+
+  it('should get all items', async () => {
+    const menu = await loader.getHarness(MatMenuHarness.with({triggerText: 'Settings'}));
+    await menu.open();
+    console.log(await menu.getItems());
+    
+    expect((await menu.getItems()).length).toBe(4);
+  })
+
+  it('should get disabled state', async () => {
+    const [enabledMenu, disabledMenu] = await loader.getAllHarnesses(MatMenuHarness);
+    expect(await enabledMenu.isDisabled()).toBe(false);
+    expect(await disabledMenu.isDisabled()).toBe(true);
+  });
+    
+
+  });
